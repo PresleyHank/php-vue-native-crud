7d5bae491e95daf58b2cca1a52b8152d5543b1b1
alexeycrystal <alex.shabramov@gmail.com>
2017-09-09 17:11:09 +0300

    Update - vue.js group add fixing (part 1)

---
+++ tests/app/dao/UserDaoImplTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ramos
+ * Date: 09.09.2017
+ * Time: 16:11
+ */
+
+namespace Dao;
+
+use PHPUnit\Framework\TestCase;
+
+class UserDaoImplTest extends TestCase
+{
+
+}
---
+++ tests/app/util/FormValidatorTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ramos
+ * Date: 09.09.2017
+ * Time: 16:13
+ */
+
+namespace Util;
+
+use PHPUnit\Framework\TestCase;
+
+class FormValidatorTest extends TestCase
+{
+
+}
